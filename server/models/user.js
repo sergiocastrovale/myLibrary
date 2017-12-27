d6487b315905a974e85982c3070e9c19302ffc2f
Sergio Castro <sergio.castro.vale@gmail.com>
2017-12-27 17:06:51 +0000

    Switched bcrypt for bcryptjs

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 import { Model } from 'objection'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 export default class User extends Model {
   // Always hash passwords before inserting
